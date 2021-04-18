e691e2f8da21dca8f56a41b169f124652a1e378b
Tanya800 <43965892+Tanya800@users.noreply.github.com>
2021-04-18 11:31:37 +0300

    fix highlighting
    fixed highlighting of the map and regions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const $regionLinks = document.querySelectorAll('.list-group a'),
+const $regionLinks = document.querySelectorAll('.region a'),
 	$mapLinks = document.querySelectorAll('.map a');
 const standStyle=`fill:#c2f8ff;stroke:#000000;stroke-width:2;stroke-linecap:round;stroke-linejoin:round;paint-order:markers stroke fill`;
 const chosenColor="#63edff";
@@ -6,9 +6,9 @@ const chosenColor="#63edff";
 $mapLinks.forEach(el => {
 	el.addEventListener('mouseenter', (e) => {
 		let self = e.currentTarget;
-		let color = self.dataset.color;
 		let selfClass = self.getAttribute('href');
-		let currentElement = document.querySelector(`.list-group a[href="${selfClass}"]`);
+		let color = self.dataset.color;
+		let currentElement = document.querySelector(`.region a[href="${selfClass}"]`);
 		let currentPath = self.querySelectorAll('path');
 		if (currentPath) currentPath.forEach(el => el.style.cssText = `fill: ${chosenColor}; stroke:#000000;stroke-width: 3px;`);
 		currentElement.classList.add('active');
@@ -17,10 +17,17 @@ $mapLinks.forEach(el => {
 	el.addEventListener('mouseleave', (e) => {
 		let self = e.currentTarget;
 		let selfClass = self.getAttribute('href');
-		let currentElement = document.querySelector(`.list-group a[href="${selfClass}"]`);
+		let currentElement = document.querySelector(`.region a[href="${selfClass}"]`);
 		let currentPath = self.querySelectorAll('path');
 		if (currentPath) currentPath.forEach(el => el.style.cssText = standStyle);
 		currentElement.classList.remove('active');
+	});
+
+	el.addEventListener('click', (e) => {
+		e.preventDefault();
+		let self = e.currentTarget;
+		let selfClass = self.getAttribute('href');
+		
 	});
 });
 
